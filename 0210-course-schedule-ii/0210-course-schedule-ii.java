f99ed61317f8923656585c6b4b685ca969f5cdc9
Mohith1220 <mohitham20@gmail.com>
2026-08-29 10:53:42 +0530

    Time: 4 ms (93.35%), Space: 47 MB (29.77%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -8,12 +8,12 @@ class Solution {
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
         }
-
+//finding indegree
         for (int[] p : prerequisites) {
             graph[p[1]].add(p[0]);
             indegree[p[0]]++;
         }
-
+//travel thorugh the graph to find which has 0 degree
         Queue<Integer> queue = new ArrayDeque<>();
 
         for (int i = 0; i < numCourses; i++) {
@@ -24,20 +24,20 @@ class Solution {
 
         int[] result = new int[numCourses];
         int index = 0;
-
+//finding the final results
         while (!queue.isEmpty()) {
-            int course = queue.poll();
+            int course = queue.poll();//pop out the top elements
             result[index++] = course;
 
             for (int next : graph[course]) {
-                indegree[next]--;
+                indegree[next]--; //on poping out of the parent node reduce the indegree
 
                 if (indegree[next] == 0) {
-                    queue.offer(next);
+                    queue.offer(next);// after reducing only if indegree is 0 push the coruse into the queue
                 }
             }
         }
 
-        return index == numCourses ? result : new int[0];
+        return index == numCourses ? result : new int[0];// check whether result macthes with number of courses if yes return res orlse return empty [] 
     }
 }
